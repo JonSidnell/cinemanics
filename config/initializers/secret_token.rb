31f026d38c549c86886d7426f2c9d9c442ab0ada
Jonathan Sidnell <me@jonsidnell.com>
2011-07-17 14:13:09 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cinemanics::Application.config.secret_token = '969da61512e39937b367ba13825c2b564e7b7cc64c6b1bdace857ea8aa8d71ddd9c088a6f057b699a78654a1edff7eb1ac9329972c3eb6732954a7206aec5fb9'
